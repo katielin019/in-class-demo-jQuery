a26121d044b470e8e71d88d695d65f028fc8efcc
Katie Lin <katielin019@gmail.com>
2023-10-02 12:14:18 -0400

    Added demo code and updated package.json to incl server

---
+++ demo.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+
+    let currentColor = 'white';
+
+    $('#btnDemo').click(() => {
+        if (currentColor === 'white') {
+            currentColor = 'blue'
+        } else {
+            currentColor = 'white'
+        }
+    })
+
+    $('#header').css('background-color', currentColor);
+    $('#header').text('Class Demo - ' + currentColor);
+})
