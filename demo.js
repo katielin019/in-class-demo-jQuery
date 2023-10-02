e5d8a1612e173352a9089115e589c57f293d4f3a
Katie Lin <katielin019@gmail.com>
2023-10-02 13:03:12 -0400

    Stopping for now. Reverted demo.js to prev state

--- demo.js
+++ demo.js
@@ -1,18 +1,16 @@
-(function ($) {
-    $(document).ready( () => {
+$(document).ready( () => {
 
-        // Do something when DOM is ready
-    
-        let currentColor = 'white'; // have to define as a semi-global var
-        $('#btnDemo').click( () => {
-            if (currentColor === 'white') {
-                currentColor = 'blue';
-            } else {
-                currentColor = 'white';
-            }
-    
-            $('#header').css('background-color', currentColor);
-            $('#header').text('Class Demo - ' + currentColor);
-        })
-    });
-}) (jQuery);
+    // Do something when DOM is ready
+
+    let currentColor = 'white'; // have to define as a semi-global var
+    $('#btnDemo').click( () => {
+        if (currentColor === 'white') {
+            currentColor = 'blue';
+        } else {
+            currentColor = 'white';
+        }
+
+        $('#header').css('background-color', currentColor);
+        $('#header').text('Class Demo - ' + currentColor);
+    })
+});
