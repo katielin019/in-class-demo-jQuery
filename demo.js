9b221f1cf1c435c47747b1ab5b9b3f923f547f30
Katie Lin <katielin019@gmail.com>
2023-10-02 13:01:23 -0400

    Tried wrapping code in another function - https://drupal.stackexchange.com/questions/89501/custom-jquery-code-loaded-but-not-working (didn't work)

--- demo.js
+++ demo.js
@@ -1,20 +1,18 @@
-$(document).ready( function() {
+(function ($) {
+    $(document).ready( () => {
 
-    // Do something when DOM is ready
-
-    let currentColor = 'white'; // have to define as a semi-global var
-    $('#btnDemo').click( () => {
-        if (currentColor === 'white') {
-            currentColor = 'blue';
-        } else {
-            currentColor = 'white';
-        }
-
-        $('#header').css('background-color', currentColor);
-        $('#header').text('Class Demo - ' + currentColor);
-    })
-});
-
-function test() {
-    console.log("button works!");
-}
+        // Do something when DOM is ready
+    
+        let currentColor = 'white'; // have to define as a semi-global var
+        $('#btnDemo').click( () => {
+            if (currentColor === 'white') {
+                currentColor = 'blue';
+            } else {
+                currentColor = 'white';
+            }
+    
+            $('#header').css('background-color', currentColor);
+            $('#header').text('Class Demo - ' + currentColor);
+        })
+    });
+}) (jQuery);
